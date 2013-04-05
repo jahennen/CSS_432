77fd66df67bd52f9560be056e2c13d8c93073548
Jay Hennen <jahennen28@hotmail.com>
2013-04-04 18:19:55 -0700

    things

---
+++ HW1/src/Stest.cpp
@@ -0,0 +1,18 @@
+/*
+ * Stest.cpp
+ *
+ *  Created on: Apr 4, 2013
+ *      Author: jay
+ */
+
+#include "Stest.h"
+
+Stest::Stest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Stest::~Stest() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ HW1/src/Stest.h
@@ -0,0 +1,17 @@
+/*
+ * Stest.h
+ *
+ *  Created on: Apr 4, 2013
+ *      Author: jay
+ */
+
+#ifndef STEST_H_
+#define STEST_H_
+
+class Stest {
+public:
+	Stest();
+	virtual ~Stest();
+};
+
+#endif /* STEST_H_ */
