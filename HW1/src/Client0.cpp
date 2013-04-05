38c8258d11813302114316848c4202775f5b37a3
Jay Hennen <jahennen28@hotmail.com>
2013-04-05 00:57:17 -0700

    THIS SHIT IS WACK AS FUCK

--- HW1/src/Client0.cpp
+++ HW1/src/Client0.cpp
@@ -1,4 +1,13 @@
 #include "Socket.h"
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netinet/in.h>   // htonl, htons, inet_ntoa
+#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close
+#include <string.h>       // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <sys/uio.h>      // writev
 
 using namespace std;
 
@@ -13,18 +22,29 @@ int main( int argc, char* argv[] ) {
 
     // establish a connection through a socket
 
-    Socket socket( port );
-    int fd = socket.getClientSocket( ip_addr );
 
-    cout << "Socket at fd " << fd <<  endl;
+    // Get server info
+
+    struct hostent* host = gethostbyname( ip_addr );
+
+    sockaddr_in sendSockAddr;
+    bzero( (char *) &sendSockAddr, sizeof( sendSockAddr ) );
+    sendSockAddr.sin_family      = AF_INET; // Address Family Internet
+    sendSockAddr.sin_addr.s_addr =
+    		inet_addr( inet_ntoa( *(struct in_addr *) *host->h_addr_list ) );
+    sendSockAddr.sin_port        = htons( port );  // convert host byte-order
+
+    int clientSd = socket( AF_INET, SOCK_STREAM, 0 );
+
+    connect( clientSd, (sockaddr *) &sendSockAddr, sizeof( sendSockAddr ) );
 
     // send a string to the server
     char* msg = "Test";
-    write( fd, msg, strlen( msg ) );
+    write( clientSd, msg, strlen( msg ) );
 
     char buffer[16];
     bzero( buffer, sizeof( buffer ) );
-    int count = read( fd, buffer, 15 );
+    int count = read( clientSd, buffer, 15 );
 
     cout << "Confirming: " << buffer << endl;
 
--- HW1/src/Server0.cpp
+++ HW1/src/Server0.cpp
@@ -1,4 +1,13 @@
 #include "Socket.h"
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netinet/in.h>   // htonl, htons, inet_ntoa
+#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close
+#include <string.h>       // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <sys/uio.h>      // writev
 
 using namespace std;
 
@@ -10,22 +19,66 @@ int main( int argc, char* argv[] ) {
 
     cout << "Port: " << port << endl;
 
-    // establish a connection through a socket
 
-    Socket socket( port );
-    int fd = socket.getServerSocket();
+    //Declare a sockaddr_in structure, zero-initialize it by calling bzero(), and set its data members as follows (htonl() converts an unsigned long from host byte order to network byte order):
 
-    cout << "Socket at fd " << fd <<  endl;
+        sockaddr_in acceptSockAddr;
+        bzero( (char *) &acceptSockAddr, sizeof( acceptSockAddr ) );
+        acceptSockAddr.sin_family      = AF_INET; // Address Family Internet
+        acceptSockAddr.sin_addr.s_addr = htonl( INADDR_ANY );
+        acceptSockAddr.sin_port        = htons( port );
 
-    // receive a string from the client
+    //Open a stream-oriented socket() with the Internet address family.
 
-    char buffer[16];
-    bzero( buffer, sizeof( buffer ) );
-    int count = read( fd, buffer, 15 );
+        int serverSd = socket( AF_INET, SOCK_STREAM, 0 );
 
-    cout << "Read: '" << buffer << "'" << endl;
+    //Set the SO_REUSEADDR option (defined in the socket() interface).
+    //(Note this option is useful to prompt OS to release the server port as soon as your server process is terminated.)
 
-    write( fd, buffer, count );
+        const int on = 1;
+        setsockopt( serverSd, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof( int ) );
+
+    /*Bind this socket to its local address by calling bind() as passing the following arguments:
+        the socket descriptor (serverSd)
+        the sockaddr_in structure defined above
+        the size of the sockaddr_in structure
+        */
+
+        bind( serverSd, (sockaddr *) &acceptSockAddr, sizeof( acceptSockAddr ) );
+
+    //Instruct the operating system to listen for up to 5 client connection requests at a time by calling listen().
+
+        listen( serverSd, 5 );
+
+    //Receive a request from a client by calling accept(), which will return a new socket descriptor specific to each connection request.
+
+        sockaddr_in newSockAddr;
+        socklen_t newSockAddrSize = sizeof( newSockAddr );
+        int newSd = accept( serverSd, (sockaddr *) &newSockAddr, &newSockAddrSize );
+
+    /*Use the read() system call to receive data from the client. (Use newSd rather than serverSd in the above code example.)
+    Use the write() system call to send back a response to the client. (Use newSd rather than serverSd in the above code example.)
+    Close the socket by calling close().
+    */
+
+        // establish a connection through a socket
+
+        Socket socket( port );
+        int fd = socket.getServerSocket();
+
+        cout << "Socket at fd " << fd <<  endl;
+
+        // receive a string from the client
+
+        char buffer[16];
+        bzero( buffer, sizeof( buffer ) );
+        int count = read( fd, buffer, 15 );
+
+        cout << "Read: '" << buffer << "'" << endl;
+
+        write( fd, buffer, count );
+
+        close( newSd );
 
     return 0;
 }
