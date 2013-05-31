f4abbed8fec8ac1c171dacecebbd95ae092980fb
Jay Hennen <jahennen28@hotmail.com>
2013-05-30 19:10:55 -0700

    added to main

--- FTP/src/ftp.cpp
+++ FTP/src/ftp.cpp
@@ -1,5 +1,21 @@
 
+#include "Socket.h"
+
+using namespace std;
+
 void main(int argc, char* argv[]) {
-	
+	Socket ctrl;
+	Socket data;
+
+	string cmd;
+	while(1) {
+		cin >> cmd;
+		switch (cmd) {
+		case "quit":
+
+			break;
+
+		}
+	}
 }
 //
