c107f0253901e2bc7182fae739a7ba74ec207ea0
toth <toth@tinybehemoth>
2013-06-02 14:33:25 -0700

    Cases Added, close and quit written

--- FTP/src/ftp.cpp
+++ FTP/src/ftp.cpp
@@ -1,21 +1,47 @@
 
 #include "Socket.h"
+#include "Timer.h"
 
 using namespace std;
 
 void main(int argc, char* argv[]) {
-	Socket ctrl;
-	Socket data;
+	int ctrl;	//Socket constructor returns an int to the location of the socket
+	int data;
 
 	string cmd;
 	while(1) {
 		cin >> cmd;
 		switch (cmd) {
-		case "quit":
 
+		case "open":
+			break;
+
+		case "password":
+			break;
+
+		case "cd":
+			break;
+
+		case "ls":
+			break;
+
+		case "get":
+			break;
+
+		case "put":
+			break;
+
+		case "close":
+			close(data);
+			break;
+
+		case "quit":
+			exit(0);
 			break;
 
 		}
 	}
+
+
 }
 //
